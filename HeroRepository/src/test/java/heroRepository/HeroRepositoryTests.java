b3cd60b4f3dc7bd7c8542dd4a7970d413516b7d9
Petya Dimitrova <101112384+PetyaaDimitrova@users.noreply.github.com>
2022-07-27 16:02:55 +0300

    Delete HeroRepositoryTests.java

--- HeroRepository/src/test/java/heroRepository/HeroRepositoryTests.java
+++
@@ -1,8 +0,0 @@
-package heroRepository;
-
-import org.junit.Assert;
-import org.junit.Test;
-
-public class HeroRepositoryTests {
-    //TODO: TEST ALL THE FUNCTIONALITY OF THE PROVIDED CLASS HeroRepository
-}
